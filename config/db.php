80a23b335634c72ba8068ed3580d4c34917d7f8f
jackieriel <jonahjacksonj@gmail.com>
2023-01-01 00:15:51 +0100

    Initial setup

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii_blog',
+    'username' => 'root',
+    'password' => '@aksu2015',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
---
+++ config/params.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'adminEmail' => 'jonahjackson@gmail.com',
+    'senderEmail' => 'jackieriel@gmail.com',
+    'senderName' => 'Jonah Jackson',
+];
